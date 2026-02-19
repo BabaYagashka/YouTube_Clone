8473e4d0c7cba58141c2ec0367b4858ecc2fe56e
BabaYagashka <virajshinde2005@gmail.com>
2026-02-19 14:58:36 +0530

    start of backend template + user controllers & routes added

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//cors
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//set a limit + configure
+//json -
+app.use(express.json({ limit: "20kb" }));
+//url -
+app.use(express.urlencoded({ limit: "20kb", extended: true }));
+//express static
+app.use(express.static("public"));
+//cookie parser
+app.use(cookieParser());
+
+// import routes -
+import userRouter from "./routes/user.route.js";
+
+// routes declaration -
+app.use("/api/v1/users", userRouter);
+
+export { app };
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./.env" });
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  });
+
+
+/* Approach 1 of connecting to atlas
+
+import express from "express"
+const app = express()
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error" , (error) => {
+        console.log(error) 
+        throw error
+    })
+    app.listen(process.env.PORT , () => {
+        console.log(`App listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();
+
+*/
