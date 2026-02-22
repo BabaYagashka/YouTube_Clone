06f1ab5b16eeb00c2180ff2c0a2dae849b46e3c1
BabaYagashka <virajshinde2005@gmail.com>
2026-02-22 20:45:14 +0530

    refactor: streamline MongoDB connection logic and remove commented code

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
+// dotenv.config({ path: "./.env" });
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(`Server running on port ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//     throw error;
+//   });
+
 dotenv.config({ path: "./.env" });
+
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
+    console.log("MongoDB connected!");
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
-    throw error;
   });
 
+export default app;
 
 /* Approach 1 of connecting to atlas
 
