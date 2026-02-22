e04399d8592badb5cbdf6a463f50f8a0584a9885
BabaYagashka <virajshinde2005@gmail.com>
2026-02-22 11:46:34 +0530

    add tweet functionality with routes and controller for creating, fetching, updating, and deleting tweets

--- src/app.js
+++ src/app.js
@@ -27,11 +27,13 @@ import userRouter from "./routes/user.route.js";
 import commentRouter from "./routes/comment.route.js";
 import videoRouter from "./routes/video.route.js";
 import likeRouter from "./routes/like.route.js";
+import tweetRouter from "./routes/tweet.route.js";
 
 // routes declaration -
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 export { app };
