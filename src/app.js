a02abf69c5114b75284c68ad1163fac86dbca0f4
BabaYagashka <virajshinde2005@gmail.com>
2026-02-22 11:38:08 +0530

    add like functionality with routes and controller for video, comment, and tweet

--- src/app.js
+++ src/app.js
@@ -26,11 +26,12 @@ app.use(cookieParser());
 import userRouter from "./routes/user.route.js";
 import commentRouter from "./routes/comment.route.js";
 import videoRouter from "./routes/video.route.js";
-
+import likeRouter from "./routes/like.route.js";
 
 // routes declaration -
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/likes", likeRouter);
 
 export { app };
